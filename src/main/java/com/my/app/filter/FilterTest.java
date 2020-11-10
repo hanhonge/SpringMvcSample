4971762ff3c0f5fd94e5505805e0eb53b0a3f3b6
권한혁 <@host.docker.internal>
2020-11-11 00:42:09 +0900

    + filter 추가(2020.11.11)   - web.xml 에 filter 등록   - root-context.xml 에 filter bean 등록

--- src/main/java/com/my/app/filter/FilterTest.java
+++ src/main/java/com/my/app/filter/FilterTest.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Component("filterTest")
 public class FilterTest implements Filter {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
